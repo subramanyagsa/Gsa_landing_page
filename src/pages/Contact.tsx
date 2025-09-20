e1f31ada1a1e1323c82d4c9efff2873c35026cfa
[dyad] <pranathib01@gmail.com>
2025-09-20 11:06:16 +0530

    [dyad] Removed the map from the Contact Us page - wrote 1 file(s)

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -87,22 +87,6 @@ const ContactPage = () => {
           </div>
         </div>
       </section>
-
-      {/* Map Section */}
-      <section className="w-full">
-        <div className="container px-4 md:px-6 pb-16 md:pb-24">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.617544236114!2d-73.98784668459399!3d40.74844097932787!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259a9b3117469%3A0xd134e199a405a163!2sEmpire%20State%20Building!5e0!3m2!1sen!2sus!4v1672533054321!5m2!1sen!2sus"
-            width="100%"
-            height="450"
-            style={{ border: 0 }}
-            allowFullScreen={false}
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-            className="rounded-lg"
-          ></iframe>
-        </div>
-      </section>
     </main>
   );
 };
