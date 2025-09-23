44c8151dee26cea49d406d0c9d2f972a25df5711
[dyad] <pranathib01@gmail.com>
2025-09-23 10:58:58 +0530

    [dyad] Removed non-existent FeaturesSection import - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import HeroSection from '@/components/HeroSection';
-import FeaturesSection from '@/components/FeaturesSection';
+// import FeaturesSection from '@/components/FeaturesSection'; // Removed import
 import ServicesSection from '@/components/ServicesSection';
 import TestimonialsSection from '@/components/TestimonialsSection';
 // import CallToActionSection from '@/components/CallToActionSection'; // Removed import
@@ -9,7 +9,7 @@ const IndexPage = () => {
   return (
     <main>
       <HeroSection />
-      <FeaturesSection />
+      {/* <FeaturesSection /> */} {/* Removed usage */}
       <ServicesSection />
       <TestimonialsSection />
       {/* <CallToActionSection /> */} {/* Removed usage */}
