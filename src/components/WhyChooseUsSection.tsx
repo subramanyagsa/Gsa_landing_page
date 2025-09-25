b18b74cac1d219159b437e7a2e1529a4b416fef5
[dyad] <pranathib01@gmail.com>
2025-09-25 18:05:30 +0530

    [dyad] Centered the grid layout in the Why Choose Us section - wrote 1 file(s)

--- src/components/WhyChooseUsSection.tsx
+++ src/components/WhyChooseUsSection.tsx
@@ -57,7 +57,7 @@ const WhyChooseUsSection = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
           {whyChooseUsPoints.map((point, index) => (
             <Card
               key={point.title}
