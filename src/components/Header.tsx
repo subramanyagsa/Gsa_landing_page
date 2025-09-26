183b7fbe91c62f6b8a64519f7bc11aeeee5e2ec3
[dyad] <pranathib01@gmail.com>
2025-09-26 15:10:46 +0530

    [dyad] Update website logo - wrote 2 file(s), deleted 2 file(s)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -78,7 +78,7 @@ const Header = () => {
         <div className="hidden md:flex items-center flex-1">
           <Link to="/" onClick={() => handleNavClick("/")}>
             <img
-              src="/logo1.png"
+              src="/GSA-LOGO(BLACK).jpg"
               alt="Global Scale Accountants Logo"
               className={cn(
                 "h-10 transition-all duration-300 filter invert brightness-200", // Added filter classes to make it white
