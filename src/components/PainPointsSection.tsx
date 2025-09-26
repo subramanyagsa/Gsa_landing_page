c8f0582b0b45b94e569e4fe3ba449d8c2473d35a
[dyad] <ankitraitalks@gmail.com>
2025-09-26 17:50:06 +0530

    Updated src/components/PainPointsSection.tsx

--- src/components/PainPointsSection.tsx
+++ src/components/PainPointsSection.tsx
@@ -39,7 +39,7 @@ const PainPointsSection = () => {
     "Confusing compliance deadlines & penalties",
     "Sky-high cost of hiring in-house finance staff",
     "Hours wasted on tax filings, payroll & messy spreadsheets",
-    "Distraction from Growth"
+    "Stuck in the back office instead of"
   ];
 
   return (
